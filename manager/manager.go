08cccedd4481ee9cbae71464ef77eead9eaacecb
Vitalii Chura <vcvitaly@yahoo.com>
2025-04-02 07:34:31 +0300

    draft worker and manager

--- manager/manager.go
+++ manager/manager.go
@@ -1,1 +1,16 @@
 package manager
+
+import (
+	"Cube/task"
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+)
+
+type Manager struct {
+	Pending       queue.Queue
+	TaskDB        map[string]*task.Task
+	EventDb       map[string]*task.TaskEvent
+	Workers       []string
+	WorkerTaskMap map[string][]uuid.UUID
+	TaskWorkerMap map[uuid.UUID]string
+}
--- worker/worker.go
+++ worker/worker.go
@@ -1,1 +1,28 @@
 package worker
+
+import (
+	"Cube/task"
+	"fmt"
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+)
+
+type Worker struct {
+	Name      string
+	Queue     queue.Queue
+	Db        map[uuid.UUID]*task.Task
+	TaskCount int
+}
+
+func (w *Worker) CollectStats() {
+	fmt.Println("I will collect stats")
+}
+func (w *Worker) RunTask() {
+	fmt.Println("I will start or stop a task")
+}
+func (w *Worker) StartTask() {
+	fmt.Println("I will start a task")
+}
+func (w *Worker) StopTask() {
+	fmt.Println("I will stop a task")
+}
